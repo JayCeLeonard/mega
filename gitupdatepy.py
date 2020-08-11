6e16a015afdef25e001bd77f0a8dbee5ed8d7f5e
JayCeLeonard <56059443+JayCeLeonard@users.noreply.github.com>
2020-08-11 16:57:16 -0700

    Update gitupdatepy.py

--- gitupdatepy.py
+++ gitupdatepy.py
@@ -1,4 +1,4 @@
 import os
-os.system('cmd /k "gitup"')#more testing
+os.system('gitup')#more testing
 
-os.system('cmd /k "git merge -m"standard update"')
+os.system('git merge -m"standard update"')
