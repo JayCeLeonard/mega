a4c42d97e12d4aaad6541acbfece8c93d657537d
JayCeLeonard <56059443+JayCeLeonard@users.noreply.github.com>
2020-08-14 16:44:32 -0700

    Update blinky.py
    work pls

--- mega/guistuff/blinky.py
+++ mega/guistuff/blinky.py
@@ -3,7 +3,7 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
 GPIO.setup(7,GPIO.OUT)
-#did it work?
+# if this does not work I swear to god
 value =1 ^ GPIO.input(7)
 print(value)
 
